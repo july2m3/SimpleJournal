10377e0f2c702b429b4d0b1895ab228a2ce0ac8d
july2m3 <july2m3@gmail.com>
2020-05-13 14:36:36 -0500

    Setup containers for day or night journal forms

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,55 @@ import React from 'react';
 
 import './style.css';
 
-const App = () => {
-  return (
-    <div className="app">
-      <h1>Simple Journal</h1>
-    </div>
-  );
+const Morning = () => {
+  return <h1>morning</h1>;
 };
+
+const Night = () => {
+  return <h1>night</h1>;
+};
+
+interface Props {}
+interface State {
+  isMorning: Boolean;
+}
+
+class App extends React.Component<Props, State> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      isMorning: true,
+    };
+  }
+
+  switchDayTime = (e: any) => {
+    console.log(e);
+    console.log(e.target);
+    console.log(e.target.value);
+    this.setState((prevState) => ({
+      isMorning: !prevState.isMorning,
+    }));
+  };
+
+  render() {
+    return (
+      <div className="app">
+        <h1>Simple Journal</h1>
+
+        {this.state.isMorning && <Morning />}
+        {!this.state.isMorning && <Night />}
+
+        <div className="buttons">
+          <button className="morning-button" onClick={this.switchDayTime}>
+            Morning
+          </button>
+          <button className="night-button" onClick={this.switchDayTime}>
+            Night
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
